810b8a3d41bb5fa60ac3b1c5f500d5d17d95d2d4
quirkles <al.quirk@gmail.com>
2023-11-17 21:07:34 -0500

    style login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,9 +2,10 @@
 
 import {useEffect} from "react";
 import {redirect} from "next/navigation";
+
+import "./login.scss"
 import {Button} from "../../components";
 import {useOauthContext} from "../../services/lichess/OAuthProvider";
-import {activeUserSlice, selectActiveUserLichessAccessToken, useSelector} from "../../redux";
 import {useOauthService} from "../hooks/useOauth";
 import {Colors} from "../../colors";
 
@@ -17,13 +18,16 @@ export default function Login() {
         }
     }, [accessToken])
     return (
-        <div>
-            <div>
-                <Button
-                    color={Colors.BLUE}
-                    text="Login"
-                    onClick={oauthService.beginAuthFlow.bind(oauthService)}
-                />
+        <div id="login-page" className="grid grid-cols-3 grid-rows-1 auto-rows-max auto-cols-max">
+            <div className="col-start-2 justify-self-stretch justify-center items-center self-center col-span-1 flex flex-col">
+                <h1 className="text-white uppercase border-4 border-white rounded py-8 px-12 m-4 text-2xl font-bold">Sign in with lichess to play</h1>
+                <div>
+                    <Button
+                        color={Colors.BLUE}
+                        text="Login"
+                        onClick={oauthService.beginAuthFlow.bind(oauthService)}
+                    />
+                </div>
             </div>
         </div>
     )
