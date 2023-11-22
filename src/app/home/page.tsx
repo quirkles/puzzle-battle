bac21a100a4bc518434b355d8c6c1c8db70f54d5
jwellwood <joewellwood@gmail.com>
2023-11-22 21:46:19 +0100

    add codegen for graphql types

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,74 +1,80 @@
-'use client'
-import {EventHandler, MouseEvent, useEffect} from "react";
-import {ApolloProvider, useMutation} from "@apollo/client";
+'use client';
+import { EventHandler, MouseEvent, useEffect } from 'react';
+import { ApolloProvider, useMutation } from '@apollo/client';
 
-import {redirect} from "next/navigation";
+import { redirect } from 'next/navigation';
 
-import {useOauthContext} from "../../services";
+import { useOauthContext } from '../../services';
 import {
-    activeUserSlice,
-    fetchLichessAccountInfo,
-    selectActiveUserLichessData,
-    useDispatch,
-    useSelector
-} from "../../redux";
-import {Button, Header} from "../../components";
-import {useOauthService} from "../hooks";
-import {LOGIN_USER} from "../../services/graphql/mutations/loginUser";
-import {apolloClient} from "../../services/graphql";
-
+  activeUserSlice,
+  fetchLichessAccountInfo,
+  selectActiveUserLichessData,
+  useDispatch,
+  useSelector,
+} from '../../redux';
+import { Button, Header } from '../../components';
+import { useOauthService } from '../hooks';
+import { LOGIN_USER } from '../../services/graphql/mutations/loginUser';
+import { apolloClient } from '../../services/graphql';
 
 export default function Home() {
-    const {oauthService} = useOauthContext()
-    const {accessToken, logout} = useOauthService(oauthService)
-    const {username, userId, puzzleRating} = useSelector(selectActiveUserLichessData)
-    const dispatch = useDispatch()
-    const doLogout = () => {
-        logout()
-        dispatch(activeUserSlice.actions.logoutLichessUser())
+  const { oauthService } = useOauthContext();
+  const { accessToken, logout } = useOauthService(oauthService);
+  const { username, userId, puzzleRating } = useSelector(
+    selectActiveUserLichessData
+  );
+  const dispatch = useDispatch();
+  const doLogout = () => {
+    logout();
+    dispatch(activeUserSlice.actions.logoutLichessUser());
+  };
+  useEffect(() => {
+    if (accessToken) {
+      if (!(username && userId)) {
+        dispatch(fetchLichessAccountInfo(accessToken.value));
+      }
+    } else {
+      redirect('./login');
     }
-    useEffect(() => {
-        if(accessToken) {
-            if(!(username && userId)) {
-                dispatch(fetchLichessAccountInfo(accessToken.value))
-            }
-        } else {
-            redirect('./login')
-        }
-    }, [accessToken, userId, username])
-            return (
-            <ApolloProvider client={apolloClient}>
-                {(userId && username) ? <HomeLoggedIn logout={doLogout}/>  : <div>Loading</div>}
-            </ApolloProvider>
-        )
+  }, [accessToken, userId, username]);
+  return (
+    <ApolloProvider client={apolloClient}>
+      {userId && username ? (
+        <HomeLoggedIn logout={doLogout} />
+      ) : (
+        <div>Loading</div>
+      )}
+    </ApolloProvider>
+  );
 }
 
-interface HomeLoggedInProps{
-    logout: EventHandler<MouseEvent<HTMLButtonElement>>
+interface HomeLoggedInProps {
+  logout: EventHandler<MouseEvent<HTMLButtonElement>>;
 }
 function HomeLoggedIn(props: HomeLoggedInProps) {
-    const {username, userId, puzzleRating} = useSelector(selectActiveUserLichessData)
-    const [loginUser, { data, loading, error }] = useMutation(LOGIN_USER)
-    useEffect(() => {
-        if(username && userId && puzzleRating) {
-            console.log("Here")
-        loginUser({
-            variables: {
-                userData: {
-                "lichessId": userId,
-                "lichessUsername": username,
-                "lichessPuzzleRating": puzzleRating
-                }
-            }
-        })
-        }
-    }, [username, userId, puzzleRating]);
-    return(
-        <>
-            <Header>
-                <span>Hello {username}</span>
-                <Button text="Logout" onClick={props.logout} color={'red'}></Button>
-            </Header>
-        </>
-    )
+  const { username, userId, puzzleRating } = useSelector(
+    selectActiveUserLichessData
+  );
+  const [loginUser, { data, loading, error }] = useMutation(LOGIN_USER);
+  useEffect(() => {
+    if (username && userId && puzzleRating) {
+      loginUser({
+        variables: {
+          userData: {
+            lichessId: userId,
+            lichessUsername: username,
+            lichessPuzzleRating: puzzleRating,
+          },
+        },
+      });
+    }
+  }, [username, userId, puzzleRating]);
+  return (
+    <>
+      <Header>
+        <span>Hello {username}</span>
+        <Button text='Logout' onClick={props.logout} color={'red'}></Button>
+      </Header>
+    </>
+  );
 }
--- src/services/graphql/mutations/loginUser.ts
+++ src/services/graphql/mutations/loginUser.ts
@@ -1,11 +1,13 @@
-import {gql} from "@apollo/client";
+import { gql } from '../../../__generated__';
 
-export const LOGIN_USER = gql`mutation CreateUser($userData: CreateUserArgs!) {
-    createUser  (userData: $userData){
-        username,
-        lichessId,
-        lichessUsername,
-        lichessPuzzleRating,
-        id
+export const LOGIN_USER = gql(/* GraphQL */ `
+  mutation CreateUser($userData: CreateUserArgs!) {
+    createUser(userData: $userData) {
+      username
+      lichessId
+      lichessUsername
+      lichessPuzzleRating
+      id
     }
-}`
+  }
+`);
