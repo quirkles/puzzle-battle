56f7412437d0f109ca4699d832f0a52ea496e4e4
quirkles <al.quirk@gmail.com>
2023-11-23 09:40:18 -0500

    init the header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import {activeUserSlice, selectActiveUserLichessId, useDispatch, useSelector} from "../../redux";
-import {Button} from "../Button";
 import {PropsWithChildren} from "react";
 
 interface HeaderProps extends PropsWithChildren{
@@ -9,8 +7,34 @@ interface HeaderProps extends PropsWithChildren{
 
 export function Header(props: HeaderProps) {
     return (
-        <header>
-            {props.children}
+        <header className="bg-white lg:py-8">
+            <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <nav className="relative flex items-center justify-between h-16 bg-white lg:rounded-md lg:shadow-lg lg:h-24 lg:px-8 lg:py-6">
+                    {props.children}
+                </nav>
+            </div>
         </header>
+    )
+}
+
+export function Left({children}: PropsWithChildren){
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+export function Center({children}: PropsWithChildren){
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+export function Right({children}: PropsWithChildren){
+    return (
+        <div>
+            {children}
+        </div>
     )
 }
---
+++ src/components/Header/className.ts
@@ -0,0 +1,3 @@
+export function getHeaderClassName(): string {
+    return ``
+}
